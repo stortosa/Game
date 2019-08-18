a6ab5598d0149158ee987436ade193442e26d718
sergio <stortosa01@gmail.com>
2019-08-18 17:56:13 +0200

    doing the player

---
+++ js/player.js
@@ -0,0 +1,93 @@
+class Player {
+  constructor(w, h, ctx) {
+    this.canvasW = w
+    this.canvasH = h
+    this.ctx = ctx
+    this.keys = {
+      arrowUp: 38,
+      spaceBar: 32,
+    }
+    this.x = this.canvasW * 0.08
+    // guardar posición original (suelo)
+    this.y0 = this.canvasH * 0.8;
+    this.y = this.y0;
+
+    this.img = new Image();
+    this.img.src = "img/ProtaDefinitivo.png";
+
+    // número de imágenes diferentes
+    this.img.frames = 5;
+    this.img.frameIndex = 0;
+
+    // medidas de la imagen a representar en el canvas
+    this.w = 63;
+    this.h = 93;
+
+    this.vy = 1;
+
+    this.bullets = [];
+
+    this.setListeners();
+  }
+
+  drawPlayer = (framesCounter) => {
+    this.ctx.drawImage(
+      this.img,
+      this.img.frameIndex * Math.floor(this.img.width / this.img.frames),
+      0,
+      Math.floor(this.img.width / this.img.frames),
+      this.img.height,
+      this.x,
+      this.y,
+      this.w,
+      this.h
+    );
+
+    this.animateImg(framesCounter);
+
+  }
+
+  animateImg = (framesCounter) => {
+    // se va cambiando el frame. Cuanto mayor es el módulo, mas lento se mueve el personaje
+    if (framesCounter % 5 === 0) {
+      this.img.frameIndex += 1;
+
+      // Si el frame es el último, se vuelve al primero
+      if (this.img.frameIndex > 4) this.img.frameIndex = 0;
+    }
+  }
+
+  setListeners = () => {
+    document.onkeydown = function (event) {
+      if (event.keyCode === this.keys.TOP_KEY && this.y == this.y0) {
+        this.y -= 5;
+        this.vy -= 10;
+      } else if (event.keyCode == this.keys.SPACE) {
+        this.shoot();
+      }
+    }.bind(this);
+  }
+
+  shoot = () => {
+    var bullet = new Bullet(
+      this.x + this.w,
+      this.y + this.h / 2,
+      this.y0,
+      this.h,
+      this.ctx
+    );
+
+    this.bullets.push(bullet);
+  }
+
+  drawBullet = () => {
+    this.bullets = this.bullets.filter(bullet => {
+      return bullet.x < this.canvasW;
+    });
+
+    this.bullets.forEach(function (bullet) {
+      bullet.draw();
+      bullet.move();
+    });
+  }
+}
